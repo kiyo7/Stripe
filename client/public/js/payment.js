a7404877598fb6970529e47061ad186bd91c7723
kiyo7 <kiyo324783@icloud.com>
2021-09-14 14:35:59 +0900

    [ADD] フロントエンド実装完了

--- client/public/js/payment.js
+++ client/public/js/payment.js
@@ -39,11 +39,14 @@ card.on('change', ({ error }) => {
 
 const submitButton = document.getElementById('payment-form-submit');
 
-submitButton.addEventListener('click', function (event) {
+//注文確定ボタンクリック時の発火イベント
+submitButton.addEventListener('click', (e) => {
+  displaySpinner();
   stripe
     .createPaymentMethod('card', card)
-    .then(function (result) {
+    .then((result) => {
       if (result.error) {
+        onError();
       } else {
         order.paymentMethodId = result.paymentMethod.id;
         fetch('http://localhost:3000/v1/order/payment', {
@@ -51,11 +54,133 @@ submitButton.addEventListener('click', function (event) {
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(order),
         })
-          .then(function (result) {
+          .then((result) => {
             return result.json();
           })
-          .then(function (response) {});
+          .then((res) => {
+            onComplete(res);
+          });
       }
     })
-    .catch(function () {});
+    .catch(() => {
+      onError();
+    });
 });
+
+//リセット処理
+
+/**
+ * リセット関数
+ * @param event
+ */
+const reset = (e) => {
+  hideError();
+  hideMessage();
+  hideNotYetMessage();
+  displayButton();
+
+  card.mount('#card-element');
+};
+let returnButtonNormal = document.getElementById('return-button-normal');
+let returnButtonError = document.getElementById('return-button-error');
+let returnButtonNotYet = document.getElementById('return-button-not-yet');
+let returnButtonDefault = document.getElementById('return-button-default');
+
+returnButtonNormal.addEventListener('click', reset);
+returnButtonError.addEventListener('click', reset);
+returnButtonNotYet.addEventListener('click', reset);
+returnButtonDefault.addEventListener('click', reset);
+
+const onComplete = (res) => {
+  shutdown();
+  hideSpinner();
+
+  if (res.error) {
+    onError();
+  } else if (res.paymentIntentStatus === 'succeeded') {
+    displayMessage();
+  } else {
+    displayNotYetMessage();
+  }
+};
+
+const onError = () => {
+  shutdown();
+  if (
+    !document.querySelector('.spinner-border').classList.contains('collapse')
+  ) {
+    hideSpinner();
+  }
+  displayError();
+};
+
+const shutdown = () => {
+  card.unmount();
+  hideButton();
+};
+
+/**********************************************************************************/
+
+//スピナー
+const hideSpinner = () => {
+  document.querySelector('.spinner-border').classList.add('collapse');
+};
+
+const displaySpinner = () => {
+  document.querySelector('.spinner-border').classList.remove('collapse');
+};
+
+/**********************************************************************************/
+
+/**********************************************************************************/
+
+// エラーメッセージ(2種類)
+const hideError = () => {
+  document.querySelector('.contents-payment-error').classList.add('collapse');
+};
+
+const displayError = () => {
+  document
+    .querySelector('.contents-payment-error')
+    .classList.remove('collapse');
+};
+
+const hideNotYetMessage = () => {
+  document.querySelector('.contents-payment-not-yet').classList.add('collapse');
+};
+
+const displayNotYetMessage = () => {
+  document
+    .querySelector('.contents-payment-not-yet')
+    .classList.remove('collapse');
+};
+
+/**********************************************************************************/
+
+/**********************************************************************************/
+
+// 成功メッセージ
+const hideMessage = () => {
+  document.querySelector('.contents-payment-result').classList.add('collapse');
+};
+
+const displayMessage = () => {
+  document
+    .querySelector('.contents-payment-result')
+    .classList.remove('collapse');
+};
+
+/**********************************************************************************/
+
+/**********************************************************************************/
+
+//注文確定ボタン
+const hideButton = () => {
+  document.querySelector('#payment-form-submit').classList.add('collapse');
+};
+
+const displayButton = () => {
+  document.querySelector('#payment-form-submit').classList.remove('collapse');
+};
+
+/**********************************************************************************/
